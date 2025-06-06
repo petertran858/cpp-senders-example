262f0ecd484d77be9a6730dce8e2cd681506d9bc
Peter Tran <ptran3000@gmail.com>
2025-06-06 10:34:50 -0700

    ex01: example using static_thread_pool for io

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # cpp-senders-example
+
+Experiments with C++ Senders/Receivers.
+
+## Examples
+
+### ex01
+
+This example uses a [static_thread_pool](https://github.com/NVIDIA/stdexec/blob/main/include/exec/static_thread_pool.hpp) for scheduling IO.
+
+Build:
+```
+cmake -B build && cmake --build ./build
+```
+
+Run:
+```
+./build/ex01/ex01
+```
+
+#### Example Output
+
+```
+after write: qsize, 1
+after read: qsize, 0
+process frame index: 0
+after write: qsize, 1
+after read: qsize, 0
+process frame index: 1
+after write: qsize, 1
+after read: qsize, 0
+process frame index: 2
+```
+
+The variable `count` controls number of iterations for `repeat_effect_until`. It is expected to be decremented every iteration until it reaches 0.
+
+However, there is currently a bug where the count never reaches 0.
+
+# References
+* [P2300](https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2024/p2300r10.html)
+    * Senders proposal accepted for C++26
+* [stdexec](https://github.com/NVIDIA/stdexec)
+    * Senders reference implementation
+* [What are Senders Good For, Anyway?](https://ericniebler.com/2024/02/04/what-are-senders-good-for-anyway/)
+    * Article by Eric Niebler showing how to wrap a C-style callback in a sender
---
+++ ex01/main.cpp
@@ -0,0 +1,165 @@
+#include <iostream>
+
+#include <exec/async_scope.hpp>
+#include <exec/repeat_effect_until.hpp>
+#include <exec/single_thread_context.hpp>
+#include <exec/static_thread_pool.hpp>
+
+/// A mock HW decoder.
+struct hw_decoder
+{
+    struct client_data_t {
+    };
+
+    using callback_t = std::function<void(client_data_t*, int)>;
+
+    // simulate a HW decoder's async callback
+    void decode_next_frame(client_data_t* clientData, callback_t on_frame_cb) {
+        auto s1 =
+            ctx.get_scheduler().schedule()
+            | stdexec::then([=, this]{
+                on_frame_cb(clientData, index++);
+            })
+            ;
+
+        scope.spawn(std::move(s1));
+    }
+
+    ~hw_decoder() {
+        stdexec::sync_wait(scope.on_empty());
+    }
+
+    exec::single_thread_context ctx;
+    exec::async_scope scope;
+    int index {};
+};
+
+// Opstate that is the bridge between C++ senders and C-style callback.
+template <class Receiver>
+struct decode_frame_op_state : hw_decoder::client_data_t {
+    using operation_state_concept = stdexec::operation_state_t;
+
+    // C-style callback registered with hw_decoder
+    static void on_frame(hw_decoder::client_data_t* baseOp, int frameIndex) {
+        auto op = static_cast<decode_frame_op_state*>(baseOp);
+        stdexec::set_value(std::move(op->receiver), frameIndex);
+    }
+ 
+    void start() noexcept {
+        // initiate async operation
+        decoder->decode_next_frame(this, &on_frame);
+    }
+
+    Receiver receiver;
+    hw_decoder* decoder;
+};
+
+struct frame_index_sender_t {
+    using sender_concept = stdexec::sender_t;
+
+    using completion_signatures = stdexec::completion_signatures<
+        stdexec::set_value_t(int),
+        stdexec::set_error_t(std::exception_ptr)>;
+
+    template <stdexec::receiver_of<completion_signatures> Receiver>
+    auto connect(Receiver&& __receiver) const {
+        return decode_frame_op_state<std::decay_t<Receiver>>
+            {
+                .receiver = std::forward<Receiver>(__receiver),
+                .decoder = decoder
+            };
+    }
+
+    hw_decoder* decoder;
+};
+
+// factory suitable for use in `let_value`
+stdexec::sender auto async_decode_frame(hw_decoder* decoder) {
+    return frame_index_sender_t { .decoder = decoder };
+}
+
+struct frame_index_cache {
+    auto read() {
+        auto lock = std::unique_lock(mutex);
+
+        signal.wait(lock, [&, this] { 
+            return !queue.empty();
+       });
+
+        auto frameIndex = queue.front();
+        queue.pop();
+        signal.notify_all();
+        std::cout << "after read: qsize, " << queue.size() << std::endl;
+        return frameIndex;
+    }
+
+    auto write(int frameIndex) {
+        auto lock = std::unique_lock(mutex);
+
+        queue.push(frameIndex);
+        signal.notify_all();
+        std::cout << "after write: qsize, " << queue.size() << std::endl;
+    }
+
+    std::mutex mutex;
+    std::condition_variable signal;
+    std::queue<int> queue;
+};
+
+int main() {
+    auto io_pool = exec::static_thread_pool(8);
+    auto io_sched = io_pool.get_scheduler();
+    auto main_scope = exec::async_scope();
+    std::atomic_bool stop_reader_requested { false };
+    
+    auto decoder = hw_decoder();
+    auto frame_cache = frame_index_cache();
+
+    int count = 100;
+
+    auto frame_decode_and_cache =
+        io_sched.schedule()
+
+        | stdexec::let_value([&] {
+            return async_decode_frame(&decoder);
+        })
+
+        | stdexec::then([&frame_cache](int frameIndex) {
+            frame_cache.write(frameIndex);
+        })
+
+        // repeat for `count` iterations
+        | stdexec::then([&count] {
+            return --count == 0;
+        })
+        | exec::repeat_effect_until()
+
+        | stdexec::then([&stop_reader_requested] {
+            //
+            // !!BUG!!: execution never reaches this point
+            //
+
+            stop_reader_requested.store(true);
+        })
+        ;
+
+    auto frame_reader =
+        io_sched.schedule()
+        | stdexec::let_value([&frame_cache] {
+            return stdexec::just(frame_cache.read());
+        })
+
+        | stdexec::then([](int frameIndex){
+            std::cout << "process frame index: " << frameIndex << std::endl;
+        })
+
+        | stdexec::then([&stop_reader_requested] { return stop_reader_requested.load(); })
+        | exec::repeat_effect_until()
+        ;
+
+    main_scope.spawn(std::move(frame_decode_and_cache));
+    main_scope.spawn(std::move(frame_reader));
+    stdexec::sync_wait(main_scope.on_empty());
+
+    return 0;
+}
